21f983cd793c42b72d5179035c37dc1d4885a712
Morgan Hallgren <morgan.hallgren@upptec.se>
2015-01-09 22:46:04 +0100

    add uglify and compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require("webpack");
+var CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   entry: __dirname + "/src/reactize.js",
@@ -10,7 +11,15 @@ module.exports = {
       // Force HTMLtoJSX to use the in-browser `document` object rather than
       // require the Node-only "jsdom" package.
       IN_BROWSER: true
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new CompressionPlugin({
+            asset: "{file}.gz",
+            algorithm: "gzip",
+            regExp: /\.js$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
   ],
   output: {
     path: __dirname + "/public/dist",
